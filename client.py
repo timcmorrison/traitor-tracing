62835e38e0f610fc3caba94b810f3adc127268c5
Tim Morrison <morrison.timc@gmail.com>
2022-10-29 10:59:11 -0600

    Created files with basic outline

---
+++ client.py
@@ -0,0 +1,1 @@
+# Connect to server with key
---
+++ server.py
@@ -0,0 +1,5 @@
+# Start server, listen for connections
+
+# If client tries connecting, check if key is valid
+
+# If key is valid, see if key is in use
