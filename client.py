00e1f0435de5445e65fd8ba505a0679d30a8dcae
Tim Morrison <morrison.timc@gmail.com>
2022-11-24 15:39:33 -0700

    Finished client/server

--- client.py
+++ client.py
@@ -1,1 +1,24 @@
-# Connect to server with key
+from http import server
+import socket
+from wsgiref.simple_server import server_version
+
+# Connect to server
+host = socket.gethostname()
+port = 12345 # The same port as used by the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+
+# Send key
+key = "veryvalidkey"
+encoded_key = key.encode('utf-8')
+s.sendall(encoded_key)
+
+# Wait for response
+server_response = False
+while not server_response:
+    data = s.recv(1024)
+    if len(data) > 1:
+        server_response = True
+
+s.close()
+print('Received', repr(data)[1:])
--- server.py
+++ server.py
@@ -1,5 +1,36 @@
+import socket
+
 # Start server, listen for connections
+host = '' # Symbolic name meaning all available interfaces
+port = 12345 # Arbitrary non-privileged port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))
+s.listen(1)
 
 # If client tries connecting, check if key is valid
+key_list = []
+key_list.append("veryvalidkey")
 
-# If key is valid, see if key is in use
+while True:
+    conn, addr = s.accept()
+    print('Connected by', addr)
+
+    key = conn.recv(1024)
+    key = key.decode('utf-8')
+    key_valid = True
+
+    if key not in key_list:
+        key_valid = False
+        msg = "Your key is not valid, try buying one bozo ;)"
+        encoded_msg = msg.encode('utf-8')
+        conn.sendall(encoded_msg)
+    else:
+        msg = "Your key is valid, thanks for being a totally legit paying customer"
+        encoded_msg = msg.encode('utf-8')
+        conn.sendall(encoded_msg)
+
+    while key_valid:
+        data = conn.recv(1024)
+        if not data: break
+        conn.sendall(data)
+    conn.close()
