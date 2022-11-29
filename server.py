982956c15a27482ec68b920759f0ce6a1137fa9e
Tim Morrison <morrison.timc@gmail.com>
2022-11-28 17:04:30 -0700

    Fixed bug with invalid keys + minor quality improvements

--- server.py
+++ server.py
@@ -3,8 +3,9 @@ from _thread import *
 
 
 # Thread function
+# TODO: Could probably clean up repeated code, but works fine
 def client_thread(client, key_list):
-    # Check if key is valid
+    # Receive key from client, perform initial check
     key = client.recv(1024)
     key = key.decode('utf-8')
     key_valid = True
@@ -14,11 +15,17 @@ def client_thread(client, key_list):
             encoded_msg = msg.encode('utf-8')
             client.sendall(encoded_msg)
             key_valid = False
+            client.close()
+
+    # Key is in the list, so it is valid
     else:
         msg = "Key valid"
         encoded_msg = msg.encode('utf-8')
         client.sendall(encoded_msg)
         key_list[key] = key_list[key] + 1
+        key_valid = True
+
+    # Initial check is done
     # Continue checking as client phones home
     while key_valid:
         key = client.recv(1024)
@@ -29,17 +36,23 @@ def client_thread(client, key_list):
             encoded_msg = msg.encode('utf-8')
             client.sendall(encoded_msg)
             key_valid = False
+            client.close()
+
         # This value corresponds to the maximum number of uses for each key; 
-        # Arbitrarily chose 2
+        # Chose 2 for demonstration purposes (4 terminals on screen, 1 server + 3 clients)
         elif key_list[key] > 2:
             msg = "Key invalid"
             encoded_msg = msg.encode('utf-8')
             client.sendall(encoded_msg)
             key_valid = False
+            del key_list[key]
+            client.close()
+            
         else:
             msg = "Key valid"
             encoded_msg = msg.encode('utf-8')
             client.sendall(encoded_msg)
+
 
 def main():
     # Start server, listen for connections on an arbitrary port
@@ -50,7 +63,12 @@ def main():
     server.bind((host, port))
     server.listen(3)
 
-    # Key: license key; Value: count of simultaneous connections using the key
+    # ASCII art "Server" to make it easily identifiable during demo
+    startup_msg = "███████╗███████╗██████╗ ██╗   ██╗███████╗██████╗ \n██╔════╝██╔════╝██╔══██╗██║   ██║██╔════╝██╔══██╗\n███████╗█████╗  ██████╔╝██║   ██║█████╗  ██████╔╝\n╚════██║██╔══╝  ██╔══██╗╚██╗ ██╔╝██╔══╝  ██╔══██╗\n███████║███████╗██║  ██║ ╚████╔╝ ███████╗██║  ██║\n╚══════╝╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝"
+    print(startup_msg)
+
+    # Key: license key
+    # Value: count of simultaneous connections using the key
     key_list = {}
     key_list["veryvalidkey"] = 0
     key_list["goodkey"] = 0
